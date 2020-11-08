03caa1eb2828957e2785785af8c0d6449c7d59df
Aishwarya Shrestha <yiyasha15@gmail.com>
2020-11-08 16:59:38 +0530

    test

--- pages/write_blog.vue
+++ pages/write_blog.vue
@@ -3,10 +3,12 @@
         <v-container class="ma-24">
         <div>
             <h5>Write a blog for the community.</h5>
-            <div>Let's share our history, experience, dance philosophies, thoughts and fun event memories.</div>
+            <div>Let's share our history, experience, dance philosophies, 
+                thoughts, stories and fun event memories.</div>
             <br>
             <vue-editor v-model="content"/>
-            <VueEmoji @input="onInput" :value="myText" /> 
+            <!-- <VueEmoji @input="onInput" :value="myText" />  -->
+            <v-btn class="text-decoration-none" rounded color="indigo" dark>Post</v-btn>
         </div>
         </v-container>
     </v-app>
